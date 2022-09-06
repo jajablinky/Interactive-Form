bc77560e02e90796637345f6804feb87d3b7a8f1
jajablinky <gmillar6@gmail.com>
2022-09-05 19:42:54 -0700

    submit function working, fixing checkbox conflict for time slots

--- js/script.js
+++ js/script.js
@@ -69,16 +69,37 @@ for (let i = 0; i < allCheckbox.length; i++) {
   allCheckbox[i].addEventListener("change", (e) => {
     if (e.target.checked) {
       totalAmount += parseInt(allCheckbox[i].getAttribute("data-cost"));
-      allCheckbox[i].parentElement.focus()
-      allCheckbox[i].parentElement.classList = 'focus';
+      allCheckbox[i].parentElement.focus();
+      allCheckbox[i].parentElement.classList = "focus";
       console.log(totalAmount);
       totalAmountEl.textContent = `Total: $${totalAmount}`;
-    } else {
+    } else if (!e.target.checked) {
       totalAmount -= parseInt(allCheckbox[i].getAttribute("data-cost"));
-      allCheckbox[i].parentElement.blur()
-      allCheckbox[i].parentElement.classList = '';
+      allCheckbox[i].parentElement.blur();
+      allCheckbox[i].parentElement.classList = "";
       console.log(totalAmount);
       totalAmountEl.textContent = `Total: $${totalAmount}`;
+    }
+    if (e.target === allCheckbox[1] && allCheckbox[3].checked) {
+      allCheckbox[3].checked = false;
+      allCheckbox[3].parentElement.blur();
+      allCheckbox[3].parentElement.classList = "";
+      totalAmount -= 100;
+    } else if (e.target === allCheckbox[3] && allCheckbox[1].checked) {
+      allCheckbox[1].checked = false;
+      allCheckbox[1].parentElement.blur();
+      allCheckbox[1].parentElement.classList = "";
+      totalAmount -= 100;
+    } else if (e.target === allCheckbox[2] && allCheckbox[4].checked) {
+      allCheckbox[4].checked = false;
+      allCheckbox[4].parentElement.blur();
+      allCheckbox[4].parentElement.classList = "";
+      totalAmount -= 100;
+    } else if (e.target === allCheckbox[4] && allCheckbox[2].checked) {
+      allCheckbox[2].checked = false;
+      allCheckbox[2].parentElement.blur();
+      allCheckbox[2].parentElement.classList = "";
+      totalAmount -= 100;
     }
   });
 }
@@ -95,15 +116,15 @@ const bitcoin = document.getElementById("bitcoin");
 paypal.style.display = "none";
 bitcoin.style.display = "none";
 payWithSelect.addEventListener("change", (e) => {
-  if (e.target.value === 'credit-card') {
+  if (e.target.value === "credit-card") {
     creditCard.style.display = "inline";
     paypal.style.display = "none";
     bitcoin.style.display = "none";
-  } else if (e.target.value === 'paypal') {
+  } else if (e.target.value === "paypal") {
     creditCard.style.display = "none";
     paypal.style.display = "block";
     bitcoin.style.display = "none";
-  } else if (e.target.value === 'bitcoin') {
+  } else if (e.target.value === "bitcoin") {
     creditCard.style.display = "none";
     paypal.style.display = "none";
     bitcoin.style.display = "block";
@@ -111,70 +132,90 @@ payWithSelect.addEventListener("change", (e) => {
   console.log(e.target);
 });
 
-/* 
-  *  Form Validation:
-  *    seeing if input content is valid and giving feedback to user.
-*/
-const email = document.getElementById('email');
-const registerForActivities = document.getElementById('activities');
-const cardNumber = document.getElementById('cc-num');
-const zipCode = document.getElementById('zip');
-const cvv = document.getElementById('cvv');
-const form = document.querySelector('form');
-const nameInput = document.getElementById('name');
-const activitiesCost = document.getElementById('activities-cost');
+/*
+ *  Form Validation:
+ *    seeing if input content is valid and giving feedback to user.
+ */
+const email = document.getElementById("email");
+const registerForActivities = document.getElementById("activities");
+const cardNumber = document.getElementById("cc-num");
+const zipCode = document.getElementById("zip");
+const cvv = document.getElementById("cvv");
+const form = document.querySelector("form");
+const nameInput = document.getElementById("name");
+const activitiesCost = document.getElementById("activities-cost");
 
 /* Validator functions to test if user submitted correctly */
 function isValidName(name) {
-  return /\S+/.test(name);
-};
+  return /^\S+$/.test(name);
+}
 
 function isValidEmail(email) {
   return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
-};
+}
 
-function isValidActivity(activity){
-  return (activity !== 'Total: $0');
-};
+function isValidEmail2(email) {
+  return /^\S+$/.test(email);
+}
 
-function isValidCardNumber(number){
+function isValidActivity(activity) {
+  return activity !== "Total: $0";
+}
+
+function isValidCardNumber(number) {
   return /^[0-9]{13,16}$/.test(number);
-};
+}
 
-function isValidZipCode(zipCode){
+function isValidZipCode(zipCode) {
   return /^[0-9]{5}$/.test(zipCode);
-};
+}
 
-function isValidCVV(cvv){
+function isValidCvv(cvv) {
   return /^[0-9]{3}$/.test(cvv);
-};
+}
 
 /*  when user error/incomplete submission is true show tool tip, hide when false  */
 
 function showOrHideTip(show, element) {
   if (show) {
     element.style.display = "inherit";
-    element.parentElement.className = 'not-valid';
+    element.parentElement.className = "not-valid";
   } else {
     element.style.display = "none";
-    element.parentElement.className = 'valid';
+    element.parentElement.className = "valid";
   }
 }
 
 function showOrHideTipActivity(show, element) {
   if (show) {
     element.style.display = "inherit";
-    element.parentElement.className = 'activities not-valid';
+    element.parentElement.className = "activities not-valid";
   } else {
     element.style.display = "none";
-    element.parentElement.className = 'activities valid';
+    element.parentElement.className = "activities valid";
   }
+}
+
+function showTipForm(element) {
+  element.nextElementSibling.style.display = "inherit";
+  element.parentElement.className = "not-valid";
+}
+
+function showTipEmailBlank(element) {
+  const errorReformat = element.nextElementSibling;
+  errorReformat.nextElementSibling.style.display = "inherit";
+  element.parentElement.className = "not-valid";
+}
+
+function showTipActivity(element) {
+  element.nextElementSibling.style.display = "inherit";
+  element.parentElement.className = "activities not-valid";
 }
 
 /* create listener functions to show tooltips if information is not submitted correctly 
 using the validators */
 function createListener(validator) {
-  return e => {
+  return (e) => {
     const text = e.target.value;
     const valid = validator(text);
     const showTip = text !== "" && !valid;
@@ -185,7 +226,7 @@ function createListener(validator) {
 }
 
 function createListenerActivity(validator) {
-  return e => {
+  return (e) => {
     const text = activitiesCost.textContent;
     const valid = validator(text);
     const showTip = text !== "" && !valid;
@@ -194,18 +235,67 @@ function createListenerActivity(validator) {
   };
 }
 
-/* event listners to listen for submits using the validators and listener functions */
+/* event listners to listen for INDIVIDUAL inputs using the validators and listener functions */
 
 nameInput.addEventListener("input", createListener(isValidName));
 
+registerForActivities.addEventListener(
+  "change",
+  createListenerActivity(isValidActivity)
+);
+
 email.addEventListener("input", createListener(isValidEmail));
 
-registerForActivities.addEventListener("change", createListenerActivity(isValidActivity));
+cardNumber.addEventListener("input", createListener(isValidCardNumber));
 
-cardNumber.addEventListener('input', createListener(isValidCardNumber));
+zipCode.addEventListener("input", createListener(isValidZipCode));
 
-zipCode.addEventListener('input', createListener(isValidZipCode));
+cvv.addEventListener("input", createListener(isValidCvv));
 
-cvv.addEventListener('input', createListener(isValidCVV));
+/* event listner on form to listen for submit on ENTIRE form to prevent page for refreshing if incorrect 
+using validator functions.*/
 
-form.addEventListener('submit', createListener(isValidName));
+form.addEventListener("submit", (e) => {
+  const nameInputValue = nameInput.value;
+  const isTheInputValid = isValidName(nameInputValue);
+  if (!isTheInputValid) {
+    e.preventDefault();
+    showTipForm(nameInput);
+  }
+
+  const emailValue = email.value;
+  const isTheEmailValid = isValidEmail(emailValue);
+  const isTheEmailFilled = isValidEmail2(emailValue);
+  if (!isTheEmailValid && isTheEmailFilled) {
+    e.preventDefault();
+    showTipForm(email);
+  } else if (!isTheEmailFilled) {
+    e.preventDefault();
+    showTipEmailBlank(email);
+  }
+
+  const activityTextContent = activitiesCost.textContent;
+  const isTheActivityValid = isValidActivity(activityTextContent);
+  if (!isTheActivityValid) {
+    e.preventDefault();
+    showTipActivity(activitiesCost);
+  }
+  const cardValue = cardNumber.value;
+  const isTheCardNumberValid = isValidCardNumber(cardValue);
+  if (!isTheCardNumberValid) {
+    e.preventDefault();
+    showTipForm(cardNumber);
+  }
+  const zipCodeValue = zipCode.value;
+  const isTheZipCodeValid = isValidZipCode(zipCodeValue);
+  if (!isTheZipCodeValid) {
+    e.preventDefault();
+    showTipForm(zipCode);
+  }
+  const cvvValue = cvv.value;
+  const isTheCvvValid = isValidCvv(cvvValue);
+  if (!isTheCvvValid) {
+    e.preventDefault();
+    showTipForm(cvv);
+  }
+});
